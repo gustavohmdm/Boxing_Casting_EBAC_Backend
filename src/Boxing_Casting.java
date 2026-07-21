5545e79bc9e1ec4cbe0c6b1f28c73f1e9e327599
GustavoHMDM <gustavohmdm180506@gmail.com>
2026-07-21 15:32:49 -0300

    Primitivo para Wrapper

--- src/Boxing_Casting.java
+++ src/Boxing_Casting.java
@@ -1,20 +1,19 @@
 
 public class Boxing_Casting {
     public static void main(String[] args) {
-        Character letra = 'A';
 
-        System.out.println("Boxing: " + letra);
+        char letra1 = 'A';
 
-        char letra2 = Character.valueOf('A');
+        Character letra2 = letra1;
 
-        System.out.println("Unboxing: " + letra2);
+        System.out.println(letra2);
 
         int num1 = 1000000;
 
-        System.out.println("O valor de int é: " + num1);
+        System.out.println(num1);
 
         short num2 = (short) num1;
 
-        System.out.println("O valor de short é: " + num2);
+        System.out.println(num2);
     }
 }
